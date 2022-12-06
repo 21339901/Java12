da92cacdd0c25023f5fe4c30bef6e1618f2987c4
21339901 <113246766+21339901@users.noreply.github.com>
2022-12-07 00:18:23 +0800

    Create Class17.java

---
+++ Class17.java
@@ -0,0 +1,10 @@
+package hibiki;
+
+public class Main {
+	
+	public static void main(String[] args) {
+			System.out.println(Short.parseShort("262904713"));
+			//結果會超出short類型的儲存數值上限而產生 value out of range 的錯誤
+		}
+	}
+	
