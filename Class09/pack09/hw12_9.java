a5ee437468994e3a95ddb06d6a993b28911d15da
21339901 <113246766+21339901@users.noreply.github.com>
2022-12-06 23:53:23 +0800

    Create hw12_9.java

---
+++ Class09/pack09/hw12_9.java
@@ -0,0 +1,15 @@
+package pack9;
+import pack9.sub1.CSphere;
+import pack9.sub2.CTrapezoid;
+public class hw12_9 {
+
+	public static void main(String[] args) {
+		CSphere cs = new CSphere(3);
+		cs.show();
+		System.out.println();
+		CTrapezoid ct = new CTrapezoid(6,9,4);
+		ct.show();
+
+	}
+
+}
