0067cb13c104e5af7b492f617be241f6d9bd604e
21339901 <113246766+21339901@users.noreply.github.com>
2022-12-06 23:25:53 +0800

    Add files via upload

--- Class08/pack6/subpack1/CCircle.java
+++ Class08/pack6/subpack1/CCircle.java
@@ -1,4 +1,5 @@
-package pack6.subpack1;   
+// CCircle.java, 此檔案置於pack6\subpack1資料夾內
+package pack6.subpack1;    // 將CCircle類別納入pack6.subpack1中
 public class CCircle
 {
    public void show()
--- Class08/pack6/subpack2/CRectangle.java
+++ Class08/pack6/subpack2/CRectangle.java
@@ -1,4 +1,5 @@
-package pack6.subpack2;    
+// CRectangle.java, 此檔案置於pack6\subpack2資料夾內
+package pack6.subpack2;    // 將CRectangle類別納入pack6.subpack2中
 public class CRectangle
 {
    public void show()
